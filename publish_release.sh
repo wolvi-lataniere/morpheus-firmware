ebc4b6f6eb58664337ba59a10433a73718407d43
Aurelien VALADE <aurelien.valade@balena.io>
2022-07-22 18:27:41 +0200

    Preparing release v0.1.0

--- publish_release.sh
+++ publish_release.sh
@@ -60,4 +60,4 @@ git tag ${TAG}
 git push --tags
 
 gh login
-gh release ${TAG} --notes ${COMMENT} out/extracted/data/morpheus-serial-${TAG}.uf2
+gh release create ${TAG} --notes ${COMMENT} out/extracted/data/morpheus-serial-${TAG}.uf2
